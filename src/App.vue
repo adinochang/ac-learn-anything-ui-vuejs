e28f155a81121a678c0e26f1cb743ded54f51f85
Adino Chang <adinochang@users.noreply.github.com>
2025-08-03 12:15:13 +0800

    Begin work on app header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppHeader from './components/layout/AppHeader.vue';
+</script>
 
 <template>
-  <header>Hello </header>
+  <AppHeader></AppHeader>
+
+  <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    <div>
+      <div class="text-xl font-medium text-black">Hello</div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="p-4 bg-gray-600 text-amber-50 flex justify-between">
+        <div id="navigation" class="">
+            AC Learn Anything
+        </div>
+        <div id="user" class="flex justify-end items-center">
+            <div class="mr-4">username@domain.com here</div>
+            <button id="logout-button"
+                class="px-4 py-1 bg-red-500 text-white rounded-full font-bold shadow-md hover:bg-red-600 transition-colors duration-200">
+                Logout
+            </button>
+        </div>
+    </div>
+</template>

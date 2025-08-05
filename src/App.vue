0249d139ef3635c5e591550969628caea58e8d57
Adino Chang <adinochang@users.noreply.github.com>
2025-08-05 09:47:56 +0800

    Setup Pinia and Prettier

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
-import AppHeader from './components/layout/AppHeader.vue';
+  import AppHeader from './components/layout/AppHeader.vue';
 </script>
 
 <template>
   <AppHeader></AppHeader>
 
-  <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div
+    class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
+  >
     <div>
       <div class="text-xl font-medium text-black">Hello</div>
     </div>
   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-</style>
+<style scoped></style>
